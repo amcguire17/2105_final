e5375e6fa0a3df25548f3b75fd1ecf4a14f522ff
Amanda McGuire <aruwart001@gmail.com>
2021-06-16 09:19:09 -0600

    Add iteration 1 classes

---
+++ lib/train.rb
@@ -0,0 +1,17 @@
+class Train
+  attr_reader :name, :type, :cargo
+
+  def initialize(info)
+    @name = info[:name]
+    @type = info[:type]
+    @cargo = Hash.new(0)
+  end
+
+  def count_cars(car)
+    @cargo[car]
+  end
+
+  def add_cars(car, count)
+    @cargo[car] += count
+  end
+end
